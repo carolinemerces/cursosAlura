2c7098548af0ca6003c470590856aa8e092fe82d
Caroline das Mercês <carolinedasmerces@gmail.com>
2021-02-25 19:04:38 -0300

    var_tipos - update

--- cursoGolang/var_tipos/var_tipos.go
+++ cursoGolang/var_tipos/var_tipos.go
@@ -1,13 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main(){
 	var nome string = "Carol"
-	var idade int = 27
-	var versao float32 = 1.1
+	var idade = 27 //inferência do tipo de variável int, string, bool
+	versao := 1.1 //declaracao e inferência de variável ao mesmo tempo e de forma direta
 
 	fmt.Println("Olá", nome)
 	fmt.Println("Sua idade é", idade)
 	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("O tipo da variável versão é", reflect.TypeOf(versao))
 }
