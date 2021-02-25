132b27948eb420998238ce85c8f849bd0d905633
Caroline das Mercês <carolinedasmerces@gmail.com>
2021-02-25 18:56:37 -0300

    var_tipos

---
+++ cursoGolang/var_tipos/var_tipos.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	var nome string = "Carol"
+	var idade int = 27
+	var versao float32 = 1.1
+
+	fmt.Println("Olá", nome)
+	fmt.Println("Sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+}
